f0e7c742159e6ca8ea173564382a5804ad1a8653
rfm5nctb9w-maker <rfm5nctb9w@privaterelay.appleid.com>
2026-05-17 13:27:10 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-#kontrollerar ifall man är root.
+#Kontrollerar ifall man är root.
 if [ "$EUID" -ne 0 ]; then
-      echo "Du måste vara root för att använda scriptet"
+      echo "Du måste vara root för att kunna köra scriptet"
 fi
+
+#Lista på användare.
+for anvandare in "$@"
+do
+      #Skapar användare med hemkatalog.
+      echo "Skapar användare: $anvandare"
+      useradd -m "$anvandare"
+
+      #Skapa mappar med Documents, Downloads och Work.
+      mkdir /home/$anvandare/Documents
+      mkdir /home/$anvandare/Downloads
+      mkdir /home/$anvandare/Work
+
+      #ägare får alla rättigheter till mapparna.
+      chmod 700 /home/$anvandare/Documents
+      chmod 700 /home/$anvandare/Downloads
+      chmod 700 /home/$anvandare/Work
+
+      #Skapa välkomstmeddelande med lista på användare
+      echo "Välkomen $anvandare" > /home/$anvandare/welcome.txt
+      echo "Andra användare: " >> /home/$anvandare/welcome.txt
+      echo "$@" >> /home/$anvandare/welcome.txt
+done
+
+echo "Användaren är färdig"
