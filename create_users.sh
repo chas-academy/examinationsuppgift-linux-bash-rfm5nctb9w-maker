fbb8f532e0d663fb2a5ab9b4ab955eb6bdb633bd
rfm5nctb9w-maker <rfm5nctb9w@privaterelay.appleid.com>
2026-05-18 02:29:02 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -19,8 +19,6 @@ do
       echo "Skapar användare: $anvandare"
       useradd -m "$anvandare"
 
-      chown -R $anvandare:$anvandare /home/$anvandare
-
       #Skapa mappar med Documents, Downloads och Work.
       mkdir /home/$anvandare/Documents
       mkdir /home/$anvandare/Downloads
